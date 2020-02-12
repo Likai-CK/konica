d02f6e474b9271599a226d9199de73e8058a5a93
Likai Kuroi <likai31197@gmail.com>
2020-02-11 19:09:40 -0500

    cleanup

--- src/server/logic_handler.js
+++ src/server/logic_handler.js
@@ -5,17 +5,14 @@ January 2020
 
 Logic Handler Module
 This module contains all functions necessary to maintain the game state.
-
-
-
 */
-
 var click_number = 1; // first click, or second click?
 var id = 1; // We start the game off at 1. The client should always be sending a message with an ID above this. If not, resend missed messages.
 var lines = []; // Array containing all lines drawn already (two points).
 var points_clicked = []; // This will be a buffer to store the first point clicked, as [x,y].
 var gameover = false; // is the game over yet?
 var active_player = 1; // is player 1 or 2 currently playing?
+
 exports.test = function() {
     console.log(counter);
 };
